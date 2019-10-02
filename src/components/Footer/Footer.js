a4d5e8f059d7187808cdfb0902a82eec0878bbb1
radu200 <raduprodan200@gmail.com>
2019-10-03 00:07:10 +0100

    added new projects

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 function footer() {
   return (
     <div className="w3-container w3-dark-grey w3-padding-32 w3-margin-top w3-center">
-      <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+       <p>Follow me on <a href="https://www.linkedin.com/in/prodanradu/">LinkedIn</a> or <a href="https://github.com/radu200" >GitHub </a></p>
    </div>
  
 
--- src/service/ProjectsData.js
+++ src/service/ProjectsData.js
@@ -1,23 +1,25 @@
 import Aerlux from './../images/aerlux.png'
 import OnlineStore from './../images/shop.png'
 import BookSearch from './../images/book_search.png'
+import JobApp from '../images/recruitment-app.png'
+import Flickr from '../images/flickr.png'
 
 export const ProjectsData  = [
     {
         'title':'Online Store',
         'description':'Ecommerce shop for selling digital products including features like user roles & permissions, account,search filters, shopping cart, authentication, payment gateway paypal and stripe, dashboard with product management etc',
         'tech': ['Html5', 'handlebars', 'css/scss','node.js,','express.js', 'mysql', 'javascript', 'jquery','webpack'],
-         'demoLink':'sdnflk',
+         'demoLink':'',
          'sourceCodeLink':'https://github.com/radu200/Nodejs-Ecommerce-Store',
          'image':OnlineStore
      },
      {
          'title':'Recruitment Web App',
-         'description':'Recruitment Web App still in progress.Icludind features like candidate  search, job search, user roles & permissions,account,authentication etc.',
+         'description':'Recruitment Web App still in progress.Icluding features like candidate  search, job search, user roles & permissions, account, authentication etc.',
          'tech': ['react/redux','css/scss', 'material-ui', 'materialize-css','node.js/express.js', 'mysql', 'webpack', 'jest/enzyme' ],
-          'demoLink':'https://aerlux.md',
-          'sourceCodeLink':'',
-          'image':Aerlux
+          'demoLink':'',
+          'sourceCodeLink':'https://github.com/radu200/JobApp',
+          'image':JobApp
       },
    {
        'title':'Aerlux.com',
@@ -27,6 +29,14 @@ export const ProjectsData  = [
         'sourceCodeLink':'',
         'image':Aerlux
     },
+    {
+        'title':'Flickr App',
+        'description':'Implement a simple web front-end using React to search and display images from the flickr public API.',
+        'tech': ['react', 'css/scss', 'javascript','responsive design'],
+         'demoLink':'https://radu200.github.io/flickr-react/',
+         'sourceCodeLink':'https://github.com/radu200/flickr-react',
+         'image':Flickr
+     },
     {
         'title':'Search A Book',
         'description':'In this project I built an app with React where you can search for your favorite book and it s displays information on parallax backgo.Feed By Google Api',
