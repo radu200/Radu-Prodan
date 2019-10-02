ed8291c3d8d3cdb5109818d2a64de3196478faa0
radu200 <raduprodan200@gmail.com>
2019-10-02 02:20:07 +0100

    fixed domain path package.json

---
+++ build/precache-manifest.4e6d158c1fc18383c7704027ca406b07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674b23800228c39775ba45202dc57e3b",
+    "url": "/Radu-Prodan/index.html"
+  },
+  {
+    "revision": "282f78d4399010f29d5c",
+    "url": "/Radu-Prodan/static/css/main.890f2127.chunk.css"
+  },
+  {
+    "revision": "67ccafc20842097ff0e3",
+    "url": "/Radu-Prodan/static/js/2.54bdc91d.chunk.js"
+  },
+  {
+    "revision": "282f78d4399010f29d5c",
+    "url": "/Radu-Prodan/static/js/main.9f6d1b6f.chunk.js"
+  },
+  {
+    "revision": "28ef5ccba9b9b089f269",
+    "url": "/Radu-Prodan/static/js/runtime-main.3a36ffe7.js"
+  },
+  {
+    "revision": "027e218f86f7ce1b64d0959acb5ecfbc",
+    "url": "/Radu-Prodan/static/media/aerlux.027e218f.png"
+  },
+  {
+    "revision": "c955778f5690741fd4642ca11d9720fa",
+    "url": "/Radu-Prodan/static/media/book_search.c955778f.png"
+  },
+  {
+    "revision": "c92f4e0d6f446228eed0e1d91c6058e0",
+    "url": "/Radu-Prodan/static/media/profile-img.c92f4e0d.jpg"
+  }
+]);
