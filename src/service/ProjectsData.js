c3e65777de406932446901f040d0ac6b5410509d
radu200 <raduprodan200@gmail.com>
2019-11-25 00:48:25 +0000

    added weather-app

--- src/service/ProjectsData.js
+++ src/service/ProjectsData.js
@@ -3,8 +3,26 @@ import OnlineStore from './../images/shop.png'
 import BookSearch from './../images/book_search.png'
 import JobApp from '../images/recruitment-app.png'
 import Flickr from '../images/flickr.png'
-
+import WeatherApp from '../images/weather-app.png'
 export const ProjectsData  = [
+    {
+        'title':'Recruitment Web App',
+        'description':'Recruitment Web App still in progress.Icluding features like candidate  search, job search, user roles & permissions, account, authentication etc.',
+        'tech': ['react/redux','css/scss', 'material-ui', 'materialize-css','node.js/express.js', 'javascript', 'mysql', 'webpack', 'jest/enzyme' ],
+         'demoLink':'',
+         'sourceCodeLink':'https://github.com/radu200/JobApp',
+         'image':JobApp
+     },
+
+     {
+        'title':'Weather App',
+        'description':'Weather app that forecast weather for every 3 hours of the day',
+        'tech': ['react/redux','css/scss', 'jest' ],
+         'demoLink':'https://radu200.github.io/weatherApp/#/',
+         'sourceCodeLink':'https://github.com/radu200/weatherApp',
+         'image':WeatherApp
+     },
+
     {
         'title':'Online Store',
         'description':'Ecommerce shop for selling digital products including features like user roles & permissions, account,search filters, shopping cart, authentication, payment gateway paypal and stripe, dashboard with product management etc',
@@ -13,14 +31,6 @@ export const ProjectsData  = [
          'sourceCodeLink':'https://github.com/radu200/Nodejs-Ecommerce-Store',
          'image':OnlineStore
      },
-     {
-         'title':'Recruitment Web App',
-         'description':'Recruitment Web App still in progress.Icluding features like candidate  search, job search, user roles & permissions, account, authentication etc.',
-         'tech': ['react/redux','css/scss', 'material-ui', 'materialize-css','node.js/express.js', 'javascript', 'mysql', 'webpack', 'jest/enzyme' ],
-          'demoLink':'',
-          'sourceCodeLink':'https://github.com/radu200/JobApp',
-          'image':JobApp
-      },
    {
        'title':'Aerlux.com',
        'description':'Aerlux.com is a travel fare aggregator website and travel metasearch engine for lodging reservations.Worked as a part of a small focused team on features like : search, calendar,resposive-design etc.',
